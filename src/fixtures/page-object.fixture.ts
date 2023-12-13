60e8a78742ffba85569b9f40ae9fb7f641794931
sylwia <sylwia.szymanska@zonifero.com>
2023-12-13 14:22:08 +0100

    chore: wip extraction of fixtures

---
+++ src/fixtures/page-object.fixture.ts
@@ -0,0 +1,14 @@
+import { LoginPage } from '../pages/login.page';
+import { test as baseTest } from '@playwright/test';
+
+interface Pages {
+  loginPage: LoginPage;
+}
+
+export const pageObjectTest = baseTest.extend<Pages>({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await use(loginPage);
+  },
+});
