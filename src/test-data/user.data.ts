eb0d4e84f73168204b9ce02fb80aec78ce56a114
sylwia <sylwia.szymanska@zonifero.com>
2023-12-13 10:40:23 +0100

    chore: env for password

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,4 +1,6 @@
-export const testUser1: LoginUser = {
+import { LoginUserModel } from '../models/user.model';
+
+export const testUser1: LoginUserModel = {
   userEmail: process.env.USER_EMAIL ?? '[NOT SET]',
   userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
 };
