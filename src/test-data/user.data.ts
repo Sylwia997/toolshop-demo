f7840d14f456e64411572694f55ea0b67c3d61ff
sylwia <sylwia.szymanska@zonifero.com>
2023-12-14 15:52:21 +0100

    chore: add customer users in user model

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -4,3 +4,13 @@ export const adminUser: LoginUserModel = {
   userEmail: process.env.USER_EMAIL ?? '[NOT SET]',
   userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
 };
+
+export const customerUser1: LoginUserModel = {
+  userEmail: 'customer@practicesoftwaretesting.com',
+  userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
+};
+
+export const customerUser2: LoginUserModel = {
+  userEmail: 'customer2@practicesoftwaretesting.com',
+  userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
+};
