ff806a01a8fc225411e13b8560939d59270cf8a8
sylwia <sylwia.szymanska@zonifero.com>
2023-12-14 15:54:26 +0100

    test: register with correct data and login

---
+++ tests/integration/register.spec.ts
@@ -0,0 +1,26 @@
+/* eslint-disable playwright/expect-expect */
+import { randomUserData } from '../../src/factories/user.factory';
+import { LoginPage } from '../../src/pages/login.page';
+import { RegisterPage } from '../../src/pages/register.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify register', () => {
+  test('register with correct data and login', async ({ page }) => {
+    // Arrange
+    const registerUserData = randomUserData();
+    const registerPage = new RegisterPage(page);
+    const loginPage = new LoginPage(page);
+
+    // Act
+    await registerPage.goto();
+    await registerPage.register(registerUserData);
+
+    // Assert
+    await expect.soft(loginPage.headingTitle).toBeVisible();
+
+    await loginPage.login({
+      userEmail: registerUserData.userEmail,
+      userPassword: registerUserData.userPassword,
+    });
+  });
+});
