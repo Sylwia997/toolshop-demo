9290c2f85803d89bce91f10daf345b4d113cf8b5
sylwia <sylwia.szymanska@zonifero.com>
2023-12-14 15:51:20 +0100

    chore: add product model

---
+++ src/test-data/product.data.ts
@@ -0,0 +1,11 @@
+import { ProductModel } from '../models/product.model';
+
+export const product1: ProductModel = {
+  productName: 'Combination Pliers',
+  productPrice: '$14.15',
+};
+
+export const product2: ProductModel = {
+  productName: 'Pliers',
+  productPrice: '$12.01',
+};
