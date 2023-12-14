58509df8276b2c08a4692ed1fb57178bba2f2bc7
sylwia <sylwia.szymanska@zonifero.com>
2023-12-14 15:50:37 +0100

    chore: add register page

---
+++ src/pages/register.page.ts
@@ -0,0 +1,41 @@
+import { RegisterUserModel } from '../models/user.model';
+import { BasePage } from './base.page';
+import { Page } from '@playwright/test';
+
+export class RegisterPage extends BasePage {
+  url = '#/auth/register';
+
+  userFirstNameInput = this.page.locator('[data-test="first-name"]');
+  userLastNameInput = this.page.locator('[data-test="last-name"]');
+  userDateOfBirthInput = this.page.locator('[data-test="dob"]');
+  userAddressInput = this.page.locator('[data-test="address"]');
+  userPostcodeInput = this.page.locator('[data-test="postcode"]');
+  userCityInput = this.page.locator('[data-test="city"]');
+  userStateInput = this.page.locator('[data-test="state"]');
+  userCountryInput = this.page.locator('[data-test="country"]');
+  userPhoneInput = this.page.locator('[data-test="phone"]');
+  userEmailInput = this.page.locator('[data-test="email"]');
+  userPasswordInput = this.page.locator('[data-test="password"]');
+  registerButton = this.page.locator('[data-test="register-submit"]');
+
+  alertPopup = this.page.getByTestId('alert-popup');
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async register(registerUserData: RegisterUserModel): Promise<void> {
+    await this.userFirstNameInput.fill(registerUserData.userFirstName);
+    await this.userLastNameInput.fill(registerUserData.userLastName);
+    // await this.userDateOfBirthInput.fill(registerUserData.userDateOfBirth);
+    await this.userAddressInput.fill(registerUserData.userAddress);
+    await this.userPostcodeInput.fill(registerUserData.userPostcode);
+    await this.userCityInput.fill(registerUserData.userCity);
+    await this.userStateInput.fill(registerUserData.userState);
+    await this.userCountryInput.selectOption(registerUserData.userCountry);
+    await this.userPhoneInput.fill(registerUserData.userPhone);
+    await this.userEmailInput.fill(registerUserData.userEmail);
+    await this.userPasswordInput.fill(registerUserData.userPassword);
+    await this.registerButton.click();
+  }
+}
