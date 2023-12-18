d582fa40cb521c79122c0e3de4436c52d4795f1a
sylwia <sylwia.szymanska@zonifero.com>
2023-12-18 09:22:22 +0100

    chore: add new login data template

--- src/test-data/user.data.ts
+++ src/test-data/user.data.ts
@@ -1,7 +1,7 @@
 import { LoginUserModel } from '../models/user.model';
 
 export const adminUser: LoginUserModel = {
-  userEmail: process.env.USER_EMAIL ?? '[NOT SET]',
+  userEmail: process.env.ADMIN_EMAIL ?? '[NOT SET]',
   userPassword: process.env.USER_PASSWORD ?? '[NOT SET]',
 };
 
