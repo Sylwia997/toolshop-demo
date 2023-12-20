af60f6159b1c37e6b1eb4cb4094dabfb27308308
sylwia <sylwia.szymanska@zonifero.com>
2023-12-20 15:23:26 +0100

    wip: product lifecycle

--- tests/end-to-end/product-lifecycle.spec.ts
+++ tests/end-to-end/product-lifecycle.spec.ts
@@ -1,21 +1,31 @@
 /* eslint-disable playwright/expect-expect */
 import { randomUserData } from '@_src/factories/user.factory';
+import { AccountPage } from '@_src/pages/account.page';
 import { CheckoutPage } from '@_src/pages/checkout.page';
 import { HomePage } from '@_src/pages/home.page';
+import { LoginPage } from '@_src/pages/login.page';
 import { ProductPage } from '@_src/pages/product.page';
 import { product1 } from '@_src/test-data/product.data';
+import { customerUser1 } from '@_src/test-data/user.data';
 import test, { expect } from '@playwright/test';
 
 test.describe('Select, add to cart and buy product', () => {
   let homePage: HomePage;
   let productPage: ProductPage;
   let checkoutPage: CheckoutPage;
+  let loginPage: LoginPage;
+  let accountPage: AccountPage;
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page);
     productPage = new ProductPage(page);
     checkoutPage = new CheckoutPage(page);
+    loginPage = new LoginPage(page);
+    accountPage = new AccountPage(page);
 
+    await loginPage.goto();
+    await loginPage.login(customerUser1);
+    await accountPage.waitForPageToLoadUrl();
     await homePage.goto();
     await homePage.goToProduct(product1.productName);
   });
@@ -58,14 +68,16 @@ test.describe('Select, add to cart and buy product', () => {
       totalCartPrice.toString(),
     );
   });
-  test('user see information about logged in', async () => {
-    await checkoutPage.proceedToCheckoutButton.click();
-    const expectedText = `Hello ${await checkoutPage.userNameMenu.textContent()}, you are already logged in. You can proceed to checkout.`;
+  // test('user see information about logged in @logged', async () => {
+  //   await productPage.addToCartButton.click();
+  //   await productPage.cartIcon.click();
+  //   await checkoutPage.proceedToCheckoutButton.click();
+  //   const expectedText = `Hello ${await checkoutPage.userNameMenu.textContent()}, you are already logged in. You can proceed to checkout.`;
 
-    await expect(checkoutPage.loggedInInformation).toHaveText(expectedText);
-  });
+  //   await expect(checkoutPage.loggedInInformation).toHaveText(expectedText);
+  // });
 
-  test('user completes missing data in checkout', async () => {
+  test('user completes missing data in checkout @logged', async () => {
     const registerUserData = randomUserData();
 
     await productPage.addToCartButton.click();
@@ -74,12 +86,12 @@ test.describe('Select, add to cart and buy product', () => {
     await expect
       .soft(checkoutPage.productName)
       .toHaveText(product1.productName);
-    await checkoutPage.proceedToCheckoutButton.click();
-    await checkoutPage.proceedToCheckoutButton.click();
+    await checkoutPage.proceedToCheckoutButtonCart.click();
+    await checkoutPage.proceedToCheckoutButtonSignIn.click();
 
     await checkoutPage.stateInput.fill(registerUserData.userState);
     await checkoutPage.postcodeInput.fill(registerUserData.userPostcode);
-    await checkoutPage.proceedToCheckoutButton.click();
+    await checkoutPage.proceedToCheckoutButtonAddress.click();
   });
   test('user add product to favorites', async () => {
     await productPage.addToFavoritesButton.click();
