acf99520bc3a8c10cab7fd4f4cd52b7d7b8a0c62
sylwia <sylwia.szymanska@zonifero.com>
2023-12-20 08:22:18 +0100

    tests: add new product tests

--- tests/end-to-end/product-lifecycle.spec.ts
+++ tests/end-to-end/product-lifecycle.spec.ts
@@ -1,9 +1,12 @@
+/* eslint-disable playwright/expect-expect */
+import { randomUserData } from '../../src/factories/user.factory';
+import { AccountPage } from '../../src/pages/account.page';
 import { CheckoutPage } from '../../src/pages/checkout.page';
 import { HomePage } from '../../src/pages/home.page';
 import { LoginPage } from '../../src/pages/login.page';
 import { ProductPage } from '../../src/pages/product.page';
 import { product1 } from '../../src/test-data/product.data';
-import { adminUser } from '../../src/test-data/user.data';
+import { customerUser1 } from '../../src/test-data/user.data';
 import test, { expect } from '@playwright/test';
 
 test.describe('Select, add to cart and buy product', () => {
@@ -11,20 +14,24 @@ test.describe('Select, add to cart and buy product', () => {
   let productPage: ProductPage;
   let checkoutPage: CheckoutPage;
   let loginPage: LoginPage;
+  let accountPage: AccountPage;
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page);
     productPage = new ProductPage(page);
     checkoutPage = new CheckoutPage(page);
     loginPage = new LoginPage(page);
+    accountPage = new AccountPage(page);
 
     await loginPage.goto();
-    await loginPage.login(adminUser);
+    await loginPage.login(customerUser1);
+    await accountPage.waitForPageToLoadUrl();
     await homePage.goto();
     await homePage.goToProduct(product1.productName);
   });
   test('user can access single product', async () => {
     // Assert
+
     await expect(productPage.productName).toHaveText(product1.productName);
   });
   test('user can add product to cart', async () => {
@@ -42,7 +49,7 @@ test.describe('Select, add to cart and buy product', () => {
     //Assert
     await expect(productPage.cartQuantity).toHaveText(productQuantity);
   });
-  // eslint-disable-next-line playwright/expect-expect
+
   test('verify product price in card', async () => {
     // Arrange
     const productQuantity = 2;
@@ -55,11 +62,38 @@ test.describe('Select, add to cart and buy product', () => {
       productQuantity,
       product1.productPrice,
     );
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const totalPriceLocator = checkoutPage.getProductPrice(totalCartPrice);
 
     //Assert
-    // await expect(totalPriceLocator).toContain(totalCartPrice);
+    await expect(checkoutPage.totalProductPrice).toContainText(
+      totalCartPrice.toString(),
+    );
   });
-  //   test('verify in checkout that user is logged in', async () => {});
+  test('user see information about logged in', async () => {
+    await checkoutPage.proceedToCheckoutButton.click();
+    const expectedText = `Hello ${await checkoutPage.userNameMenu.textContent()}, you are already logged in. You can proceed to checkout.`;
+
+    await expect(checkoutPage.loggedInInformation).toHaveText(expectedText);
+  });
+
+  test('user completes missing data in checkout', async () => {
+    const registerUserData = randomUserData();
+
+    await productPage.addToCartButton.click();
+    await productPage.cartIcon.click();
+
+    await expect
+      .soft(checkoutPage.productName)
+      .toHaveText(product1.productName);
+    await checkoutPage.proceedToCheckoutButton.click();
+    await checkoutPage.proceedToCheckoutButton.click();
+
+    await checkoutPage.stateInput.fill(registerUserData.userState);
+    await checkoutPage.postcodeInput.fill(registerUserData.userPostcode);
+    await checkoutPage.proceedToCheckoutButton.click();
+  });
+  test('user add product to favorites', async () => {
+    await productPage.addToFavoritesButton.click();
+
+    await expect(productPage.addProductToFavoritesPopUp).toBeVisible();
+  });
 });
