c7a6b66cde5531b6156e15e54b49502d9d32b9d8
sylwia <sylwia.szymanska@zonifero.com>
2023-12-21 14:37:48 +0100

    chore: add function format date for cart validation

--- src/helpers/date.helper.ts
+++ src/helpers/date.helper.ts
@@ -3,3 +3,9 @@ export function formatDateForRegister(data: Date): string {
   const [year, month, day] = tmp.split('T')[0].split('-');
   return `${year}-${month}-${day}`;
 }
+export function formatDateForCartValidation(data: Date): string {
+  const tmp = data.toISOString();
+  const [month] = tmp.split('T')[0].split('-')[1];
+  const [year] = tmp.split('T')[0].split('-')[0];
+  return `${month}/${year}`;
+}
