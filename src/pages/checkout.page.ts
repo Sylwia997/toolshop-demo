f9b9c33896f21b8fc663bf32e70d9b8c2b11e59f
sylwia <sylwia.szymanska@zonifero.com>
2023-12-21 14:39:08 +0100

    chore: add new locators

--- src/pages/checkout.page.ts
+++ src/pages/checkout.page.ts
@@ -3,7 +3,9 @@ import { Page } from '@playwright/test';
 
 export class CheckoutPage extends BasePage {
   url = '#/checkout';
-  proceedToCheckoutButton = this.page.locator('.btn-success');
+  proceedToCheckoutButtonCart = this.page.locator('[data-test="proceed-1"]');
+  proceedToCheckoutButtonSignIn = this.page.locator('[data-test="proceed-2"]');
+  proceedToCheckoutButtonAddress = this.page.locator('[data-test="proceed-3"]');
   totalProductPrice = this.page.getByText('$').nth(1);
   productName = this.page.locator('.product-title');
   productQuantity = this.page.locator('.form-control quantity');
@@ -11,7 +13,9 @@ export class CheckoutPage extends BasePage {
   userNameMenu = this.page.locator('#user-menu');
   stateInput = this.page.locator('#state');
   postcodeInput = this.page.locator('#postcode');
-  loggedInInformation = this.page.locator('.col-md-6 offset-md-3 login-form-1');
+  loggedInInformationText = this.page.locator(
+    '.col-md-6 offset-md-3 login-form-1',
+  );
 
   constructor(page: Page) {
     super(page);
