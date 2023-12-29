f6352f0e21dd2e5c78bf3d017191ce5dd36a31f3
sylwia <sylwia.szymanska@zonifero.com>
2023-12-29 09:42:57 +0100

    wip: refactor in login page

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,3 +1,4 @@
+import { AccountPage } from './account.page';
 import { BasePage } from './base.page';
 import { LoginUserModel } from '@_src/models/user.model';
 import { Page } from '@playwright/test';
@@ -8,13 +9,15 @@ export class LoginPage extends BasePage {
   userPasswordInput = this.page.locator('[data-test="password"]');
   loginButton = this.page.locator('[data-test="login-submit"]');
   headingTitle = this.page.getByRole('heading', { name: 'Login' });
+  loginError = this.page.locator('[data-test="login-error"]');
   constructor(page: Page) {
     super(page);
   }
 
-  async login(loginUserData: LoginUserModel): Promise<void> {
+  async login(loginUserData: LoginUserModel): Promise<AccountPage> {
     await this.userEmailInput.fill(loginUserData.userEmail);
     await this.userPasswordInput.fill(loginUserData.userPassword);
     await this.loginButton.click();
+    return new AccountPage(this.page);
   }
 }
