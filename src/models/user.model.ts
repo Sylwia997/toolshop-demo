75cf5878759ebb9edb0d26b61af261663eb8baa2
sylwia <sylwia.szymanska@zonifero.com>
2023-12-29 09:44:02 +0100

    chore: add contact form model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -15,3 +15,12 @@ export interface LoginUserModel {
   userEmail: string;
   userPassword: string;
 }
+
+export interface ContactFormModel {
+  firstName: string;
+  lastName: string;
+  userEmail: string;
+  subject: string;
+  message: string;
+  attachment: string;
+}
